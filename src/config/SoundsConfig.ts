e491010abf883aa3e0ea3f76613c8f05f9470e68
Patrick Hammond <phammond@greentube.com>
2024-12-27 19:06:24 +0100

    Update assets path

--- src/config/SoundsConfig.ts
+++ src/config/SoundsConfig.ts
@@ -13,7 +13,7 @@ export function getAllSoundFilenames(): string[] {
 }
 
 const sounds: Record<SoundKeys, string> = {
-  [SoundKeys.MAIN_GAME_BACKGROUND]: "assets/sounds/maingame_backgroundmusic.ogg",
-  [SoundKeys.REELS_STOP]: "assets/sounds/reels_stop_1.ogg",
-  [SoundKeys.UI_SPIN_BUTTON]: "assets/sounds/ui_spinbutton.ogg"
+  [SoundKeys.MAIN_GAME_BACKGROUND]: "/dist/assets/sounds/maingame_backgroundmusic.ogg",
+  [SoundKeys.REELS_STOP]: "/dist/assets/sounds/reels_stop_1.ogg",
+  [SoundKeys.UI_SPIN_BUTTON]: "/dist/assets/sounds/ui_spinbutton.ogg"
 };
