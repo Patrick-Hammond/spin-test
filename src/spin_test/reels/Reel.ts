1fc370fe659ec2914915bf10d4016ce14667d6a6
Patrick Hammond <phammond@greentube.com>
2024-12-27 16:26:55 +0100

    Additional comments

--- src/spin_test/reels/Reel.ts
+++ src/spin_test/reels/Reel.ts
@@ -78,51 +78,6 @@ export class Reel extends GameObject {
     }
   }
 
-  private moveReel(ticker: Ticker): void {
-    // at first, I tried moving the symbol sprites rather the the whole reel
-    // but even after accounting for excess overrun, they would eventually drift in relative position
-    // also, i wanted to copy the original spin design where the symbol position remain static as the reel spins
-    // I achieved this by moving the symbol's parent container and when it has moved a symbol height distance
-    // reset it back to the start and cascade all the symbol textures down
-    // trying lower values in Constants.maxReelSpeed results in more standard spinning
-    this.reel.y += ticker.deltaMS * this.speed;
-
-    if (this.reel.y >= symbolHeight) {
-      this.reel.y = 0;
-      this.updateSymbolTexture(this.symbolSprites[0]);
-    }
-  }
-
-  private updateSymbolTexture(symbolSprite: Sprite): void {
-    switch (this.state) {
-      case ReelState.SPINNING:
-        // Change the symbol according to the reel strip
-        this.cascadeSymbolTextures();
-        if (--this.nextSymbolIndex < 0) this.nextSymbolIndex = this.reelStrip.length - 1;
-        symbolSprite.texture = getSymbolTexture(this.reelStrip[this.nextSymbolIndex], this.speed > 3);
-        break;
-
-      case ReelState.STOPPING:
-        // Change the symbol according to the landing symbols
-        // in this case we want to cascade the textures AFTER updating first symbolSprite
-        symbolSprite.texture = getSymbolTexture(this.landingSymbols.pop() as SymbolName, false);
-        this.cascadeSymbolTextures();
-        if (this.landingSymbols.length === 0) {
-          this.time = 0;
-          this.state = ReelState.BOUNCE;
-          getSound(SoundKeys.REELS_STOP).play();
-        }
-        break;
-    }
-  }
-
-  private cascadeSymbolTextures(): void {
-    //copy texture references from symbolSprite 0 -> 1, 1 -> 2 etc.. 
-    for (let i = this.symbolSprites.length - 1; i > 0; i--) {
-      this.symbolSprites[i].texture = this.symbolSprites[i - 1].texture;
-    }
-  }
-
   private startReel(ticker: Ticker): void {
     this.time += ticker.deltaMS * 0.009;
     this.reel.y = lerpExp(0, -70, this.time);
@@ -149,6 +104,7 @@ export class Reel extends GameObject {
       this.speed = 1;
     }
   }
+
   private bounceReel(ticker: Ticker): void {
     this.time += ticker.deltaMS * 0.02;
     if (this.time <= 1) {
@@ -160,12 +116,61 @@ export class Reel extends GameObject {
       this.onSpinCompleted();
     }
   }
+
+  private moveReel(ticker: Ticker): void {
+    // at first, I tried moving the symbol sprites rather the the whole reel
+    // but even after accounting for excess overrun, they would eventually drift in relative position
+    // also, i wanted to copy the original spin design from https://slotcatalog.com/en/slots/egyptian-marvel
+    // where the symbol position remain static as the reel spins
+    // I achieved this by moving the symbol's parent container and when it has moved a symbol height distance
+    // reset it back to the start and cascade all the symbol textures down
+    // lowering the Constants.maxReelSpeed value results in more traditional spinning
+    this.reel.y += ticker.deltaMS * this.speed;
+
+    if (this.reel.y >= symbolHeight) {
+      this.reel.y = 0;
+      this.updateSymbolTexture(this.symbolSprites[0]);
+    }
+  }
+
+  private updateSymbolTexture(symbolSprite: Sprite): void {
+    switch (this.state) {
+      case ReelState.SPINNING:
+        // Change the symbol according to the reel strip
+        // cascade the textures first and then set the first sprite's texture
+        this.cascadeSymbolTextures();
+        if (--this.nextSymbolIndex < 0) this.nextSymbolIndex = this.reelStrip.length - 1;
+        symbolSprite.texture = getSymbolTexture(this.reelStrip[this.nextSymbolIndex], this.speed > 3);
+        break;
+
+      case ReelState.STOPPING:
+        // Change the symbol according to the landing symbols
+        // in this case we want to cascade the textures AFTER updating top symbolSprite
+        symbolSprite.texture = getSymbolTexture(this.landingSymbols.pop() as SymbolName, false);
+        this.cascadeSymbolTextures();
+        if (this.landingSymbols.length === 0) {
+          this.time = 0;
+          this.state = ReelState.BOUNCE;
+          getSound(SoundKeys.REELS_STOP).play();
+        }
+        break;
+    }
+  }
+
+  private cascadeSymbolTextures(): void {
+    //copy texture references from symbolSprite 0 -> 1, 1 -> 2 etc.. 
+    for (let i = this.symbolSprites.length - 1; i > 0; i--) {
+      this.symbolSprites[i].texture = this.symbolSprites[i - 1].texture;
+    }
+  }
+
   private onSpinCompleted(): void {
     Ticker.shared.remove(this.update, this);
     this.reel.y = 0;
     this.state = ReelState.STOPPED;
     this.onComplete();
   }
+  
   private logLandingSymbols(landingSymbols: string[]): void {
     console.log(`Reel ${this.reelIndex} landing symbols: ${landingSymbols.slice(1, 4).join(", ")}`);
   }
